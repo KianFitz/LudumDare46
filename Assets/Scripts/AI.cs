ac3cb1f81b3d25245aef9c0e4b7c92452cb0ff6b
Dan Barton <danbarton2000@gmail.com>
2020-04-18 15:09:18 +0100

    Added the outlines of the statemachine

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class AI : MonoBehaviour
@@ -26,17 +25,35 @@ public class AI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        bool canSeePlayer = CheckForPlayer();
+
+        if (!canSeePlayer)
+            Move();
+        else
+            Shoot();
+    }
+
+    private bool CheckForPlayer() {
+        return false;
+    }
+
+    private void Shoot() {
+
+    }
+
+    private void Move() {
         var curPos = new Vector2Int((int)transform.position.x, (int)transform.position.y);
         var targetPos = new Vector2Int((int)target.transform.position.x, (int)target.transform.position.y);
 
 
         if (curPos == targetPos) {
             pointer++;
-            if (pointer > points.Count-1) pointer = 0;
+            if (pointer > points.Count - 1) pointer = 0;
 
             target = points[pointer];
             dir = (target.transform.position - transform.position).normalized;
-        }    
+        }
 
         transform.position += dir * moveSpeed * Time.deltaTime;
     }
