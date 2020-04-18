b845b229213db06760df7da4e1836454db19b929
Dan Barton <danbarton2000@gmail.com>
2020-04-18 21:58:24 +0100

    Added the ability to pick up gems and when the count gets to 0, an action can be performed.

---
+++ Assets/Scripts/Gem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    private void Start() {
+        LevelManager.Instance.AddGem(this);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        LevelManager.Instance.RemoveGem(this);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public static LevelManager Instance { get; private set; }
+
+    [SerializeField]
+    private GameObject exit;
+    [SerializeField]
+    private List<Gem> gems;
+
+    private void Awake() {
+        Instance = this;
+    }
+
+    public void AddGem(Gem gem) {
+        gems.Add(gem);
+    }
+
+    public void RemoveGem(Gem gem) {
+
+        Instance.gems.Remove(gem);
+
+        if (Instance.gems.Count == 0) {
+            // Open the exit
+            Debug.Log("Collected all the gems");
+        }
+    }
+
+}
