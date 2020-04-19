613788c7e3f9f713df65ea26de4853b84d2f7517
Dan Barton <danbarton2000@gmail.com>
2020-04-19 15:42:50 +0100

    Added spawn class so you don't have to have the character in the level. This can allow for the character to persist across levels

---
+++ Assets/Scripts/SpawnCharacter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCharacter : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject character;
+
+    private void Start() {
+        Instantiate(character);
+    }
+
+
+}
