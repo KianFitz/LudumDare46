0d4ef84dc1b613f7c37291c4fd78254113ea8a51
Dan Barton <danbarton2000@gmail.com>
2020-04-19 19:52:29 +0100

    Added a health class with a on death callback

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    private float charge;
+    public float Charge { 
+        get { 
+            return charge; 
+        }
+        set { 
+            charge = value;
+            if (charge <= 0) {
+                onDeath();
+            }
+        }
+    }
+
+    private Action onDeath;
+
+    public void SubscribeToOnDeathCallback(Action callback) {
+        onDeath += callback;
+    }   
+}
